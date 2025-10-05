230d7d077daf37c55ec82ecf792b84a9362f9824
Mattar17 <mattar17x@gmail.com>
2025-10-05 15:45:59 +0300

    cors

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const appRouter = require("./routers/app.router");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors(corsOptions));
 
 mongoose
   .connect(process.env.DEV_CONNECTION_STRING)
