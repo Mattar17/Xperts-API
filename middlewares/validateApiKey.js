79982a9ab9e0c4ea3c9129879fe65e6210139e09
Mattar17 <mattar17x@gmail.com>
2026-02-20 00:02:31 +0200

    deployment error fix

--- middlewares/validateApiKey.js
+++ middlewares/validateApiKey.js
@@ -1,11 +1,11 @@
-export default function validateApiKey(req, res, next) {
+function validateApiKey(req, res, next) {
   const apiKey = req.headers["x-api-key"];
   if (!apiKey || apiKey !== process.env.API_KEY)
-    return res
-      .status(403)
-      .json({
-        status: "error",
-        message: "Forbidden: api key is required or invalid",
-      });
+    return res.status(403).json({
+      status: "error",
+      message: "Forbidden: api key is required or invalid",
+    });
   next();
 }
+
+module.exports = validateApiKey;
